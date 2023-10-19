4d1e74f0aeacd14f51e7cf0e9ffee8b106da0c0c
presuke <reno.neve@gmail.com>
2023-10-19 13:41:18 +0900

    modified:   resources/scss/app.scss 	modified:   resources/vue/Footer.vue 	modified:   resources/vue/Header.vue

--- resources/vue/Footer.vue
+++ resources/vue/Footer.vue
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 <template>
     <div>
         Copyright &copy; 2023, presuke Allright reserved.
     </div>
-=======
-<template>
-    <div>
-        Copyright &copy; 2023, presuke Allright reserved.
-    </div>
->>>>>>> c2ed9f6025e0b5e95de41960dcaba376070db908
 </template>
