af4ba6de999726f039e2152f0d48d5f39f13a40c
presuke <reno.neve@gmail.com>
2023-10-19 13:29:48 +0900

    new file:   resources/image/avatar/male/yhmv148.png 	new file:   resources/image/avatar/male/yhmv154.png 	new file:   resources/image/avatar/male/yhmv176.png 	new file:   resources/image/avatar/male/yhmv178.png 	new file:   resources/image/avatar/male/yhmv179.png

---
+++ resources/vue/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Copyright &copy; 2023, presuke Allright reserved.
+    </div>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    outputDir:'../public/webview', // ファイルの出力先ルート
+    publicPath: './webview'
+  }
