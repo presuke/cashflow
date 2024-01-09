49e0c375eb799a3ac11a837aa2532b846f9864ee
presuke <reno.neve@gmail.com>
2024-01-09 18:13:45 +0900

    new file:   public/image/cardDesign.png 	new file:   public/image/win.png 	new file:   resources/scss/index.scss 	new file:   resources/vue/CopyRight.vue 	new file:   resources/vue/HowTo.vue

---
+++ resources/vue/HowTo.vue
@@ -0,0 +1,37 @@
+<template>
+    <h2>How To</h2>
+    <ul style="margin-left:20px;">
+        <li>
+            キャラクターアイコン：<a href="https://blog.goo.ne.jp/akarise" target="_blank">ゆうひな様</a>
+        </li>
+        <li>
+            ステータスアイコン：<a href="https://icooon-mono.com/" target="_blank">モノアイコン</a>
+        </li>
+        <li>
+            アクションイラスト：<a href="https://www.irasutoya.com/" target="_blank">いらすとや様</a>
+        </li>
+        <li>
+            サウンドエフェクト：<a href="https://soundeffect-lab.info/" target="_blank">効果音ラボ様</a>
+        </li>
+        <li>
+            画像生成AI：<a href="https://www.bing.com/images/create?FORM=GDPCLS" target="_blank">Bing Image Creator</a>
+        </li>
+    </ul>
+    <div style="font-size:smaller;">
+        ※素敵な素材、ツールのご提供に、感謝して使わせていただいております。
+    </div>
+    <h2>ソース</h2>
+    <div>
+        <img 
+        src="../image/github-mark.svg" 
+        class="icon" 
+        style="filter: drop-shadow(2px 2px 2px #66c);width:30px;height:30px;"
+        />
+        <a href="https://github.com/presuke/cashflow" target="_blank">
+            https://github.com/presuke/cashflow
+        </a>
+        <div style="font-size:smaller;">
+            ※vue+laravel勉強目的で作成したものです。非効率な記述や細かな不具合などあるかもしれません。
+        </div>
+    </div>
+</template>
